f66261a5afc53b9a3c8b0c785b2a2b6350c8ce74
anshikacodes56 <anshikasidana05@gmail.com>
2025-10-26 14:13:08 +0530

    Update Simple_Calculator.c

--- PROJECTS/Simple_Calculator.c
+++ PROJECTS/Simple_Calculator.c
@@ -2,30 +2,30 @@
 int main() 
 {
     char Operator; 
-    float NUMBER1, NUMBER2;
+    int NUMBER1, NUMBER2;
   
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &Operator); 
 
     printf("Enter two operands: ");
-    scanf("%f %f", &NUMBER1, &NUMBER2);
+    scanf("%d %d", &NUMBER1, &NUMBER2);
 
     switch (Operator) 
     {
         case '+':
             // Using %f for full float outputs
-            printf("%f + %f = %f\n", NUMBER1, NUMBER2, NUMBER1 + NUMBER2);
+            printf("%d + %d = %d\n", NUMBER1, NUMBER2, NUMBER1 + NUMBER2);
             break;
         case '-':
-            printf("%f - %f = %f\n", NUMBER1, NUMBER2, NUMBER1 - NUMBER2);
+            printf("%d - %d = %d\n", NUMBER1, NUMBER2, NUMBER1 - NUMBER2);
             break;
         case '*':
-            printf("%f * %f = %f\n", NUMBER1, NUMBER2, NUMBER1 * NUMBER2);
+            printf("%d * %d = %d\n", NUMBER1, NUMBER2, NUMBER1 * NUMBER2);
             break;
         case '/':
             if (NUMBER2 != 0) 
             {
-                printf("%f / %f = %f\n", NUMBER1, NUMBER2, NUMBER1 / NUMBER2);
+                printf("%d / %d = %d\n", NUMBER1, NUMBER2, NUMBER1 / NUMBER2);
             } 
             else 
             {
